cf2960ccc417b95e74e98b9a1eb4fab81a151f57
Deniss Ivanjuk <deniss.ivanjuk@gmail.com>
2025-07-02 20:02:13 +0300

    Initial commit
    Project scaffolding and initial file setup.

---
+++ astro.config.mjs
@@ -0,0 +1,37 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Ohtuleht",
+      logo: {
+        light: "./src/assets/light-logo.svg",
+        dark: "./src/assets/dark-logo.svg",
+        replacesTitle: true,
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/withastro/starlight",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+});
